d07db9415185d251b8c5c03cefb32b2ce01fb789
Sascha Wessel <swessel@gr4yweb.de>
2010-09-16 22:42:25 +0200

    added store_path constant

--- lib/twiauth.rb
+++ lib/twiauth.rb
@@ -6,5 +6,6 @@ require 'yaml'
 module TwiAuth
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
+  STORE_PATH = ::File.expand_path("~" + ::File::SEPARATOR + "twiauth") + ::File::SEPARATOR
   VERSION = ::File.exist?(PATH + 'VERSION') ? ::File.read(PATH+'VERSION') : ""
 end

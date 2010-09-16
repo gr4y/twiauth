014e4b77217ec19e8541f32f164cb7593237e73c
Sascha Wessel <swessel@gr4yweb.de>
2010-09-16 22:43:05 +0200

    data store moved into the home directory

--- lib/twiauth/store.rb
+++ lib/twiauth/store.rb
@@ -4,10 +4,11 @@ module TwiAuth
   # A simple key value store using PStore, provided by ruby
   class Store
     # Filename for pstore data file
-    DATA_FILE = "data.pstore"
+    STORE_FILENAME = "data.pstore"
     # :nodoc:
     def initialize
-      @store = PStore.new("#{PATH}#{DATA_FILE}")
+      prepare_store
+      @store = PStore.new(STORE_PATH + STORE_FILENAME)
     end
 
     # put a key with value
@@ -29,6 +30,25 @@ module TwiAuth
     def delete(key)
       @store.transaction do
         @store.delete(key)
+      end
+    end
+
+    private
+    # preparing the new data store
+    def prepare_store
+      if !::Dir.exists?(STORE_PATH)
+        ::Dir.mkdir(STORE_PATH)
+      end
+      if !::File.exists?(STORE_PATH + STORE_FILENAME)
+        migrate_old_store
+      end
+    end
+
+    # migrate old data store from version 0.1.0
+    def migrate_old_store
+    old_data_file =  PATH + STORE_FILENAME
+      if ::File.exists?(old_data_file)
+        ::FileUtils.move old_data_file, STORE_PATH
       end
     end
   end

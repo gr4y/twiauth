26900b2fba1c92f04916759208a6672da633ae4a
Sascha Wessel <swessel@gr4yweb.de>
2011-03-26 09:25:19 +0100

    removed store requirement from twiauth.rb

--- lib/twiauth.rb
+++ lib/twiauth.rb
@@ -1,11 +1,9 @@
 require 'oauth'
 require 'twiauth/oauth'
-require 'twiauth/store'
 require 'yaml'
 
 module TwiAuth
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-  STORE_PATH = ::File.expand_path("~" + ::File::SEPARATOR + "twiauth") + ::File::SEPARATOR
   VERSION = ::File.exist?(PATH + 'VERSION') ? ::File.read(PATH+'VERSION') : ""
 end
